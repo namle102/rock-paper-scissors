62cd0d0bf3d5b43f736547509a2c49279b59b981
Nam Le <139657367+namle102@users.noreply.github.com>
2024-01-02 22:43:34 -0500

    Add re-play button

--- app.js
+++ app.js
@@ -1,62 +1,73 @@
 // GAME ON
-let playerChoice;
-let computerChoice;
+let playerChoice, computerChoice;
 let result;
+let pCount = 0,
+    cCount = 0;
 
 const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
 
 const body = document.querySelector("body");
-const div = document.createElement("div");
 const span = document.createElement("span");
+const h2 = document.createElement("h2");
+const h1 = document.createElement("h1");
+const replay = document.createElement("button");
 
 rock.addEventListener("click", () => {
     playerChoice = "Rock";
     computerChoice = getComputerChoice();
     result = playRound(playerChoice, computerChoice);
-
-    // Display the current round
-    if (result === 1) {
-        span.textContent = "Tie";
-        span.style.color = "black";
-    } else if (result === 0) {
-        span.textContent = `You Lose! ${computerChoice} beats ${playerChoice}`;
-        span.style.color = "red";
-    } else {
-        span.textContent = `You Win! ${playerChoice} beats ${computerChoice}`;
-        span.style.color = "green";
-    }
-    div.appendChild(span);
-    body.appendChild(span);
+    displayResult();
+    displayTotal();
 });
 
 paper.addEventListener("click", () => {
     playerChoice = "Paper";
     computerChoice = getComputerChoice();
     result = playRound(playerChoice, computerChoice);
-
-    // Display the current round
-    if (result === 1) {
-        span.textContent = "Tie";
-        span.style.color = "black";
-    } else if (result === 0) {
-        span.textContent = `You Lose! ${computerChoice} beats ${playerChoice}`;
-        span.style.color = "red";
-    } else {
-        span.textContent = `You Win! ${playerChoice} beats ${computerChoice}`;
-        span.style.color = "green";
-    }
-    div.appendChild(span);
-    body.appendChild(span);
+    displayResult();
+    displayTotal();
 });
 
 scissors.addEventListener("click", () => {
     playerChoice = "Scissors";
     computerChoice = getComputerChoice();
     result = playRound(playerChoice, computerChoice);
+    displayResult();
+    displayTotal();
+});
 
-    // Display the current round
+replay.addEventListener("click", () => {
+    pCount = 0;
+    cCount = 0;
+    body.removeChild(h1);
+    body.removeChild(h2);
+    body.removeChild(span);
+    body.removeChild(replay);
+});
+
+// HELPER: Display total
+const displayTotal = () => {
+    h2.textContent = `${pCount} - ${cCount}`;
+    body.appendChild(h2);
+
+    if (pCount === 5 && cCount < 5) {
+        h1.textContent = "##### YOU WIN #####";
+        replay.textContent = "PLAY AGAIN";
+        body.appendChild(h1);
+        body.appendChild(replay);
+    }
+    if (pCount < 5 && cCount === 5) {
+        h1.textContent = "##### YOU LOSE #####";
+        replay.textContent = "PLAY AGAIN";
+        body.appendChild(h1);
+        body.appendChild(replay);
+    }
+};
+
+// HELPER: Display result
+const displayResult = () => {
     if (result === 1) {
         span.textContent = "Tie";
         span.style.color = "black";
@@ -67,21 +78,19 @@ scissors.addEventListener("click", () => {
         span.textContent = `You Win! ${playerChoice} beats ${computerChoice}`;
         span.style.color = "green";
     }
-    div.appendChild(span);
     body.appendChild(span);
-});
+};
 
-// HELPER FUNC: Randomly get a computer choice
+// HELPER: Randomly get a computer choice
 const getComputerChoice = () => {
     const choices = ["Rock", "Paper", "Scissors"];
     const choice = Math.floor(Math.random() * 3);
     return choices[choice];
 };
 
-// HELPER FUNC: Play round
+// HELPER: Play round
 const playRound = (playerChoice, computerChoice) => {
     let playerNum, computerNum;
-    let pCount, cCount;
 
     if (playerChoice === "Rock") playerNum = 0;
     else if (playerChoice === "Paper") playerNum = 1;
@@ -93,8 +102,10 @@ const playRound = (playerChoice, computerChoice) => {
     if (playerNum === computerNum) {
         return 1;
     } else if (computerNum - playerNum === 1 || playerNum - computerNum === 2) {
+        cCount++;
         return 0;
     } else {
-        return 2;
+        pCount++;
+        return 3;
     }
 };
