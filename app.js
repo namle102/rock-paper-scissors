80d22d57808ff7f090bf12c6d2b089e18692145d
Nam Le <139657367+namle102@users.noreply.github.com>
2024-01-02 21:47:33 -0500

    Make UI for a single play partially

--- app.js
+++ app.js
@@ -1,38 +1,87 @@
-// Randomly get a computer choice
+// GAME ON
+let playerChoice;
+let computerChoice;
+let result;
+
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+
+const body = document.querySelector("body");
+const div = document.createElement("div");
+const span = document.createElement("span");
+
+rock.addEventListener("click", () => {
+    playerChoice = "Rock";
+    computerChoice = getComputerChoice();
+    result = playRound(playerChoice, computerChoice);
+
+    // Display the current round
+    if (result === 1) {
+        span.textContent = "Tie";
+        span.style.color = "black";
+    } else if (result === 0) {
+        span.textContent = `You Lose! ${computerChoice} beats ${playerChoice}`;
+        span.style.color = "red";
+    } else {
+        span.textContent = `You Win! ${playerChoice} beats ${computerChoice}`;
+        span.style.color = "green";
+    }
+    div.appendChild(span);
+    body.appendChild(span);
+});
+
+paper.addEventListener("click", () => {
+    playerChoice = "Paper";
+    computerChoice = getComputerChoice();
+    result = playRound(playerChoice, computerChoice);
+
+    // Display the current round
+    if (result === 1) {
+        span.textContent = "Tie";
+        span.style.color = "black";
+    } else if (result === 0) {
+        span.textContent = `You Lose! ${computerChoice} beats ${playerChoice}`;
+        span.style.color = "red";
+    } else {
+        span.textContent = `You Win! ${playerChoice} beats ${computerChoice}`;
+        span.style.color = "green";
+    }
+    div.appendChild(span);
+    body.appendChild(span);
+});
+
+scissors.addEventListener("click", () => {
+    playerChoice = "Scissors";
+    computerChoice = getComputerChoice();
+    result = playRound(playerChoice, computerChoice);
+
+    // Display the current round
+    if (result === 1) {
+        span.textContent = "Tie";
+        span.style.color = "black";
+    } else if (result === 0) {
+        span.textContent = `You Lose! ${computerChoice} beats ${playerChoice}`;
+        span.style.color = "red";
+    } else {
+        span.textContent = `You Win! ${playerChoice} beats ${computerChoice}`;
+        span.style.color = "green";
+    }
+    div.appendChild(span);
+    body.appendChild(span);
+});
+
+// HELPER FUNC: Randomly get a computer choice
 const getComputerChoice = () => {
     const choices = ["Rock", "Paper", "Scissors"];
     const choice = Math.floor(Math.random() * 3);
     return choices[choice];
 };
 
-// Play round
-let playerChoice;
-let playerNum;
-let computerChoice;
-let computerNum;
-
-const rock = document.querySelector("#rock");
-const paper = document.querySelector("#paper");
-const scissors = document.querySelector("#scissors");
-
-rock.addEventListener("click", () => {
-    playerChoice = "Rock";
-    computerChoice = getComputerChoice();
-    console.log(playRound(playerChoice, computerChoice));
-});
-paper.addEventListener("click", () => {
-    playerChoice = "Paper";
-    computerChoice = getComputerChoice();
-    console.log(playRound(playerChoice, computerChoice));
-});
-scissors.addEventListener("click", () => {
-    playerChoice = "Scissors";
-    computerChoice = getComputerChoice();
-    console.log(playRound(playerChoice, computerChoice));
-});
-
+// HELPER FUNC: Play round
 const playRound = (playerChoice, computerChoice) => {
     let playerNum, computerNum;
+    let pCount, cCount;
 
     if (playerChoice === "Rock") playerNum = 0;
     else if (playerChoice === "Paper") playerNum = 1;
@@ -41,8 +90,11 @@ const playRound = (playerChoice, computerChoice) => {
     else if (computerChoice === "Paper") computerNum = 1;
     else computerNum = 2;
 
-    if (playerNum === computerNum) return "Tie";
-    else if (computerNum - playerNum === 1 || playerNum - computerNum === 2)
-        return `You Lose! ${computerChoice} beats ${playerChoice}`;
-    else return `You Win! ${playerChoice} beats ${computerChoice}`;
+    if (playerNum === computerNum) {
+        return 1;
+    } else if (computerNum - playerNum === 1 || playerNum - computerNum === 2) {
+        return 0;
+    } else {
+        return 2;
+    }
 };
